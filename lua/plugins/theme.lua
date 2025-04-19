c3aa0b826c8efd67611643b962ed5520d10532ac
Zaman Zulkarnaen <gavier.exe@gmail.com>
2025-04-19 19:58:29 +0700

    fix: Few floating window border conflicts with theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -7,11 +7,9 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+      vim.g.gruvbox_material_float_style = "dim"
       vim.g.gruvbox_material_background = "hard"
-      vim.g.gruvbox_material_pallete = "mix"
       vim.cmd([[colorscheme gruvbox-material]])
-      vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#1d2021", fg = "#45403d" })
-      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1d2021" })
     end,
   },
 }
