acc452f5036d53e91d2006f2c7d7c68171768065
Zaman Zulkarnaen <gavier.exe@gmail.com>
2025-04-20 11:38:19 +0700

    twk: Enable more text support

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -4,11 +4,14 @@ return {
   -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   {
     "sainnhe/gruvbox-material",
+    name = "gruvbox-material",
     lazy = false,
     priority = 1000,
     config = function()
       vim.g.gruvbox_material_float_style = "dim"
       vim.g.gruvbox_material_background = "hard"
+      vim.g.gruvbox_material_enable_bold = 1
+      vim.g.gruvbox_material_enable_italic = 1
       vim.cmd([[colorscheme gruvbox-material]])
     end,
   },
